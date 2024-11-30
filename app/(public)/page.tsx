f93c7bf7639da9de023ecb6a99ce5f7f8dbc2c0a
Roho7 <rohosen2@gmail.com>
2024-11-30 08:39:33 +0530

    fix [site seo]

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,26 +1,79 @@
-"use client";
 
-import FAQSection from "@/app/_components/_landing/faq";
-import ForBusinessesSection from "@/app/_components/_landing/for-businesses";
-import Hero from "@/app/_components/_landing/hero";
-import LogoRibbon from "@/app/_components/_landing/logo-ribbon";
-import ServicesSection from "@/app/_components/services-card";
-import { Separator } from "@/components/ui/separator";
+
+import { Metadata, Viewport } from "next";
+import MainPublicPage from "./mainpage";
+
+export const GEARYO_META_TAGS = {
+  title: 'Gearyo - Rent the perfect gear for your next adventure',
+  description:
+    'Gearyo is a platform to rent the perfect gear for your next adventure',
+  icon: 'logo-bg-white.png',
+  og_image: `${
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    'https://' + process.env.VERCEL_BRANCH_URL
+  }/brand/logo_bg_white.png`,
+  theme_color: '#192E37',
+};
+
+export const metadata: Metadata = {
+  title: {
+    template: `${GEARYO_META_TAGS.title}`,
+    default: GEARYO_META_TAGS.title, // a default is required when creating a template
+  },
+  description: `${GEARYO_META_TAGS.description}`,
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_BASE_URL ||
+      'https://' + process.env.VERCEL_BRANCH_URL ||
+      'https://gearyo.com'
+  ),
+  manifest: '/manifest.webmanifest',
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: `${GEARYO_META_TAGS.title}`,
+    description: `${GEARYO_META_TAGS.description}`,
+    url:
+      process.env.NEXT_PUBLIC_BASE_URL ||
+      'https://' + process.env.VERCEL_BRANCH_URL,
+    siteName: GEARYO_META_TAGS.title,
+    images: [
+      {
+        url: GEARYO_META_TAGS.og_image,
+        width: 1200,
+        height: 630,
+        alt: GEARYO_META_TAGS.title,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: `${GEARYO_META_TAGS.title}`,
+    description: `${GEARYO_META_TAGS.description}`,
+    creator: '@rohosen_',
+    images: [
+      {
+        url: GEARYO_META_TAGS.og_image,
+        width: 1200,
+        height: 630,
+        alt: GEARYO_META_TAGS.title,
+      },
+    ],
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: `${GEARYO_META_TAGS.theme_color}`,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+  initialScale: 1,
+};
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 px-4">
-      <Hero />
-      {/* <RecommendationSection /> */}
-      <Separator className="my-4" />
-      <LogoRibbon />
-      <Separator className="my-4" />
-      <ServicesSection />
-      <Separator className="my-4" />
-      <ForBusinessesSection />
-      <Separator className="my-4" />
-      {/* <AdCard /> */}
-      <FAQSection />
-    </div>
+    <MainPublicPage />
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Urbanist } from "next/font/google";
 import Script from "next/script";
 
-import { Metadata, Viewport } from "next";
 import { ThemeProvider } from "next-themes";
 import { LoginModal } from "./_components/_shared/login.modal";
 import { AuthProvider } from "./_providers/useAuth";
@@ -13,77 +12,6 @@ export const dynamic = "force-dynamic";
 const lato = Urbanist({
   subsets: ["latin"],
 });
-
-export const GEARYO_META_TAGS = {
-  title: 'Gearyo - Rent the perfect gear for your next adventure',
-  description:
-    'Gearyo is a platform to rent the perfect gear for your next adventure',
-  icon: 'logo-bg-white.png',
-  og_image: `${
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    'https://' + process.env.VERCEL_BRANCH_URL
-  }/brand/logo_bg_white.png`,
-  theme_color: '#192E37',
-};
-
-export const metadata: Metadata = {
-  title: {
-    template: `${GEARYO_META_TAGS.title}`,
-    default: GEARYO_META_TAGS.title, // a default is required when creating a template
-  },
-  description: `${GEARYO_META_TAGS.description}`,
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_BASE_URL ||
-      'https://' + process.env.VERCEL_BRANCH_URL ||
-      'https://gearyo.com'
-  ),
-  manifest: '/manifest.webmanifest',
-  alternates: {
-    canonical: '/',
-  },
-  openGraph: {
-    title: `${GEARYO_META_TAGS.title}`,
-    description: `${GEARYO_META_TAGS.description}`,
-    url:
-      process.env.NEXT_PUBLIC_BASE_URL ||
-      'https://' + process.env.VERCEL_BRANCH_URL,
-    siteName: GEARYO_META_TAGS.title,
-    images: [
-      {
-        url: GEARYO_META_TAGS.og_image,
-        width: 1200,
-        height: 630,
-        alt: GEARYO_META_TAGS.title,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: `${GEARYO_META_TAGS.title}`,
-    description: `${GEARYO_META_TAGS.description}`,
-    creator: '@rohosen_',
-    images: [
-      {
-        url: GEARYO_META_TAGS.og_image,
-        width: 1200,
-        height: 630,
-        alt: GEARYO_META_TAGS.title,
-      },
-    ],
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: `${GEARYO_META_TAGS.theme_color}`,
-  maximumScale: 1,
-  minimumScale: 1,
-  userScalable: false,
-  initialScale: 1,
-};
-
-
 
 export default function RootLayout({
   children,
